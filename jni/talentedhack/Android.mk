49112d8d3ae7291e4bd133c45317c244cb77b641
Ethan Chen <intervigil@gmail.com>
2010-08-10 14:29:30 -0700

    leave fftw3.h path as default

--- jni/talentedhack/Android.mk
+++ jni/talentedhack/Android.mk
@@ -36,8 +36,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := talentedhack
 LOCAL_CFLAGS := -std=c99
 LOCAL_SRC_FILES := circular_buffer.c fft.c formant_corrector.c lfo.c pitch_detector.c pitch_shifter.c pitch_smoother.c quantizer.c talentedhack.c
-LOCAL_C_INCLUDES := . \
-					$(LOCAL_PATH)/fftw3/api
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/fftw3/api
 LOCAL_STATIC_LIBRARIES := fftw3
 LOCAL_LDLIBS := -llog
 
