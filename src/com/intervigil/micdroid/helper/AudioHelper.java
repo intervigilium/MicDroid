2d0c8b9988ffc9207f56b94b3e27765dbc23277a
Ethan Chen <intervigil@gmail.com>
2010-08-10 17:43:47 -0700

    Revert "start autodetecting sample rate at 44.1khz for samsung galaxy s"
    This reverts commit ecb152c0735c99fdd8e88bfefda92a8c72e6eeb7.

--- src/com/intervigil/micdroid/helper/AudioHelper.java
+++ src/com/intervigil/micdroid/helper/AudioHelper.java
@@ -164,7 +164,8 @@ public class AudioHelper {
     		do {
     			switch (sampleRate) {
 	    			case -1:
-	    				sampleRate = Constants.SAMPLE_RATE_44KHZ;
+	    				// set the default to 22050Hz, so slower devices perform better
+	    				sampleRate = Constants.SAMPLE_RATE_22KHZ;
 	    				break;
 					case Constants.SAMPLE_RATE_44KHZ:
 						sampleRate = Constants.SAMPLE_RATE_22KHZ;
